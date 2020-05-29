86c9c07266bd9107592c39c4ccd02b0abdbe7f19
Norrismi <Norrismi0@gmail.com>
2020-05-28 23:19:24 -0400

    app is creating and reading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import './components/cardOne/index'
+import React, { Component } from "react";
+import "./components/cardOne/index";
+import axios from "axios";
 
-import './App.css';
-import CardOne from './components/cardOne/index';
+import "./App.css";
+import CardOne from "./components/cardOne/index";
 
-function App() {
-  return (
-    <div>
-      <CardOne/>
-  
-    </div>
-  );
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: "Columbus",
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { cityName, forecast } = this.state;
+
+    const URL = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${cityName}&units=f`;
+    axios
+      .get()
+      .then((res) => {
+        return (res.data);
+      })
+      .then((data) => {
+        this.setState({
+          isLoading: false,
+          temp: data.current.temperature,
+          isDay: data.current.is_day,
+          weather_desc: data.current.weather_descriptions,
+          weather_icon: data.current.weather_icons,
+          time: data.location.localtime,
+          state: data.location.region,
+          country: data.location.country
+        });
+      })
+
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+
+        <CardOne {...this.state}
+          // location={cityName}
+          // temperature={temp}
+          // is_day={isDay}
+          // weather_desc={weather_desc}
+          // weather_icon={weather_icon}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/cardOne/index.js
+++ src/components/cardOne/index.js
@@ -1,31 +1,52 @@
 import React, { Component } from "react";
 import "./style.scss";
-import Img from '../resources/moon.png'
+
 
 class panelOne extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  // handleTime = (time)  => {
+  //   let whatTime = time.split(" ").slice(1).toString();
+  //   let hour = whatTime.substring(0, 2);
+  //   let min = whatTime.substring(3, 5);
+  //   return (hour > 12) ? `${hour - 12}:${min}` : `${hour}:${min}`;
+  // }
+
   render() {
+    const {
+      cityName,
+      temp,
+      isDay,
+      weather_desc,
+      weather_icon,
+      time,
+      state,
+      country
+    } = this.props;
+    
+
     return (
       <div className="app-container">
         <div className="main-container">
           <div className="left-side">
-              <div className="location-sec">Cincinnati</div>
-              <div className="time-sec">as of 6:30 pm EST</div>
-              <div className="temp-sec">73°</div>
-              <div className="condition-sec">Cloudy</div>
+    <div className="location-sec">{cityName}, {country}</div>
+            <div className="time-sec">as of {this.handleTime} pm EST</div>
+            <div className="temp-sec">{temp}77°<div className="units-sec">F</div></div>
+            <div className="condition-sec">{weather_desc}</div>
           </div>
           <div className="right-side">
-              <img src={Img} alt="moon" className="img-sec"/>
-              <div className="high-low-sec">80°/69°</div>
+            <img src={weather_icon} alt="weather icon" className="img-sec" />
+            <div className="high-low-sec">80°/69°</div>
           </div>
         </div>
-
       </div>
     );
+    
+   
   }
+
 }
 
 export default panelOne;
